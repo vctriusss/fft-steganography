2a313d5b8cdbaeda8f4002de457b057361fb8d15
vctriusss <vskamaev@miem.hse.ru>
2024-03-19 17:10:35 +0300

    feat: got rid of paddings

--- src/fft.py
+++ src/fft.py
@@ -10,22 +10,18 @@ from src.utils import (
     get_xmap,
 )
 
-PADS = (1, 1)
-
 
 def embed(img: Image, text: str) -> Image:
     img = normalize_image(np.asarray(img))
-    print(img.shape)
-    text_img = text_to_image(text, (img.shape[1] - 2 * PADS[0], img.shape[0] // 2 - 2 * PADS[1]))
+    text_img = text_to_image(text, (img.shape[1], img.shape[0] // 2))
     text_img = normalize_image(np.asarray(text_img))
     
     img_fft = np.fft.fft2(img, None, (0, 1))
     
-    embedding_zone = np.zeros((img.shape[0] // 2 - 2 * PADS[0], 
-                               img.shape[1] - 2 * PADS[1], 
+    embedding_zone = np.zeros((img.shape[0] // 2, 
+                               img.shape[1], 
                                3))
-    print(embedding_zone.shape, text_img.shape)
-    
+
     embedding_zone[:text_img.shape[0], :text_img.shape[1]] = text_img
     
     _, low, high = centralize(img_fft)
@@ -33,8 +29,8 @@ def embed(img: Image, text: str) -> Image:
     
     xmap = get_xmap(embedding_zone.shape)
     
-    img_fft[PADS[0] + xmap[0], PADS[1] + xmap[1]] += embedding_zone * alpha
-    img_fft[-PADS[0] - xmap[0], -PADS[1] - xmap[1]] += embedding_zone * alpha
+    img_fft[xmap[0], xmap[1]] += embedding_zone * alpha
+    img_fft[-xmap[0], -xmap[1]] += embedding_zone * alpha
     
     img_inv = np.fft.ifft2(img_fft, None, (0, 1)).real
     
@@ -50,8 +46,8 @@ def extract(img: Image, base_img: Image) -> Image:
     
     img_fft -= base_img_fft
     
-    xmap = get_xmap((img.shape[0] // 2 - 2 * PADS[0], img.shape[1] - 2 * PADS[1]))
+    xmap = get_xmap((img.shape[0] // 2, img.shape[1]))
     
-    embedding_zone, _, _ = centralize(img_fft[PADS[0] + xmap[0], PADS[1] + xmap[1]])
+    embedding_zone, _, _ = centralize(img_fft[xmap[0], xmap[1]])
     
     return denormalize_image(embedding_zone)
